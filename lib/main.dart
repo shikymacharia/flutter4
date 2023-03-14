c997d95168cf0930ec5c9014c10a2300afe93bfa
shikymacharia <Mash_Agnes@AGNES MINGW64 ~>
2023-03-14 16:26:30 +0300

    flutter4

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Form Styling Demo';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const Text(
+            'e in your country because of the support of the Age of Learning Foundation'),
+      ),
+    );
+  }
+}
